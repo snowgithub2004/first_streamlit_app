00bacdf48d01f307515f49d7efd8924c62513361
snowgithub2004 <117619889+snowgithub2004@users.noreply.github.com>
2022-11-07 16:05:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My parents new health diner')
 
 
-streamlit.header('Breakfast Menu')
-
-streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
