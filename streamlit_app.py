8be688700fa437727387b6871e14b440dd9e055f
snowgithub2004 <117619889+snowgithub2004@users.noreply.github.com>
2022-11-10 07:17:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,10 @@ from urllib.error import URLError
 streamlit.title('My parents new health diner')
 
 streamlit.header('Breakfast Menu')
-
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -38,17 +34,12 @@ try:
 except URLError as e:
   streamlit.error()
 streamlit.stop()
-
-
-
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load contains:")
+streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
-
 
 add_my_fruit = streamlit.text_input('What fruit would you Add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
