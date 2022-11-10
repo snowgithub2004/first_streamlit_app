9579ef1d031969224384cd0f947f46402b4a6983
snowgithub2004 <117619889+snowgithub2004@users.noreply.github.com>
2022-11-09 21:48:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ try:
     streamlit.error("dsfasdfasf")
   else: 
     back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
     
 except URLError as e: 
     streamlit.error()
